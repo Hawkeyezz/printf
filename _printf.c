bc0dd5a3d92aa2d084a2aa7f3be8369087f515e2
EbimoJustin <136062054+EbimoJustin@users.noreply.github.com>
2023-08-22 19:04:28 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,6 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
+ *compiled by - Clinton Etuk and Ebimoboere Justin
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.

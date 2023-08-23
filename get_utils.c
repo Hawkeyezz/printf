c2f46c543dec97c1ca715b7786c8792bb2f9e4a0
EbimoJustin <136062054+EbimoJustin@users.noreply.github.com>
2023-08-22 19:14:40 -0500

    Update get_utils.c

--- get_utils.c
+++ get_utils.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ *Compiled by - Clinton Etuk and Ebimoboere Justin
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
  * Return: 1 if c is printable, 0 otherwise
